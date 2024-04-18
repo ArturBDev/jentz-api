3b27235c5049ed586a747bd8acfd1e02b0f79727
ArturBDev <165692326+ArturBDev@users.noreply.github.com>
2024-04-18 16:58:19 -0300

    Revert "Revert "updating dev""

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { PrismaModule } from "./prisma/prisma.module";
 import { ProductsModule } from "./products/products.module";
+import { MedicationsModule } from './medications/medications.module';
 
 @Module({
-  imports: [PrismaModule, ProductsModule],
+  imports: [PrismaModule, ProductsModule, MedicationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
