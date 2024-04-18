5ed675ea0e4e435b71e18525561aec29c8128dc6
Artur Santos <arturbernardo49@gmail.com>
2024-04-18 17:02:19 -0300

    fixing things

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,9 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { PrismaModule } from "./prisma/prisma.module";
-import { ProductsModule } from "./products/products.module";
-import { MedicationsModule } from './medications/medications.module';
 
 @Module({
-  imports: [PrismaModule, ProductsModule, MedicationsModule],
+  imports: [PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
