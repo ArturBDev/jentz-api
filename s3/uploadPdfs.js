f85e1235c21c793dc4a091fbb9345e4e70a3cc12
Artur Santos <arturbernardogithub@gmail.com>
2024-09-03 11:43:27 -0300

    creating script to add pdfs amd images to s3

--- s3/uploadPdfs.js
+++ s3/uploadPdfs.js
@@ -1,27 +1,35 @@
-const { PrismaClient } = require("@prisma/client");
-const AWS = require("aws-sdk");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { Upload } = require("@aws-sdk/lib-storage");
 const fs = require("fs");
 const path = require("path");
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+const s3Client = new S3Client({
   region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 });
 
 async function uploadPdfToS3(filePath, fileName) {
   const fileContent = fs.readFileSync(filePath);
 
-  const params = {
+  const uploadParams = {
     Bucket: process.env.S3_BUCKET_NAME,
     Key: fileName,
     Body: fileContent,
     ContentType: "application/pdf",
   };
 
-  const data = await s3.upload(params).promise();
+  const upload = new Upload({
+    client: s3Client,
+    params: uploadParams,
+  });
+
+  const data = await upload.done();
   return data.Location;
 }
 
@@ -34,26 +42,22 @@ async function main() {
     const fileName = path.basename(pdfFile);
 
     try {
-      const pdfUrl = await uploadPdfToS3(filePath, fileName);
+      const leafletUrl = await uploadPdfToS3(filePath, fileName);
 
       // Update the product in the database
       await prisma.product.update({
         where: { id: parseInt(fileName.split(".")[0]) }, // Assuming the file name is the product ID
-        data: { pdfUrl },
+        data: { leafletUrl },
       });
 
-      console.log(`Uploaded ${fileName} to ${pdfUrl}`);
+      console.log(`Uploaded ${fileName} to ${leafletUrl}`);
     } catch (error) {
       console.error(`Failed to upload ${fileName}:`, error);
     }
   }
 }
 
-main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
